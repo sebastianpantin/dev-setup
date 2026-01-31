f25b42ef00a94fcb20e2361944d836cbe5b95ba8
Sebastian Pantin Liljevall <sebastian.p.liljevall@captario.com>
2026-01-31 17:18:01 +0100

    refactor: remove faulty config

--- dotfiles/nvim/.config/nvim/lua/plugins/lspsettings/fsautocomplete.lua
+++
@@ -1,9 +0,0 @@
-return {
-	-- Override deprecated default settings
-	cmd = { "fsautocomplete", "--background-service-enabled" },
-	root_dir = require("lspconfig.util").root_pattern("*.sln", "*.fsproj", ".git"),
-	init_options = {
-		AutomaticWorkspaceInit = true,
-	},
-	settings = {},
-}
